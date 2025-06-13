595508aef8112f8560cbf0e752f6acf45776174a
ALAN K BIJU <119912590+ALAN-K-BIJU@users.noreply.github.com>
2025-06-13 23:10:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -1,27 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int N = sc.nextInt();
-            int M[][] = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    M[i][j] = sc.nextInt();
-                }
-            }
-            System.out.println(new Solution().celebrity(M));
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
 
 
 // User function Template for Java
